ba03dbb6620452073cc78d2cf3fff147c07397cb
pandaks <13120612606@163.com>
2018-10-22 23:10:10 +0800

    change

---
+++ chapter1/basicKnowledge.py
@@ -0,0 +1,60 @@
+# 第一个注释
+print("hello,python!")
+
+'''
+第二个注释
+'''
+
+"""
+第三种注释
+"""
+
+if True:
+    print("true")
+else:
+    print("false")
+
+stedhwidhiewhjr = 'panda'
+# 输出字符串
+print(stedhwidhiewhjr)
+
+# 输出下标位置1的字母
+print(stedhwidhiewhjr[1])
+# 输出下表2到结尾处所有的字符
+print(stedhwidhiewhjr[2:])
+
+# 输出2遍
+print(stedhwidhiewhjr * 2)
+# \n换行
+print(stedhwidhiewhjr + "55\n5")
+
+abc = 5
+print(type("1\n23"))
+print(type(233))
+print(type(abc))
+print(type(2.55))
+print(type(True))
+
+# 英文变量不能直接等于，要用双引号表示赋的是英文
+# =是赋值，==表示判断是否相同
+# 逐一判断循环语句elif
+a = "wu"
+b = "fan"
+c = "pp"
+if c == a:
+    print("wu")
+elif c == b:
+    print("fan")
+else:
+    print("Q")
+
+listpp=['a','b','c','wu','fan'];
+i = "wu"
+# 变量名index
+if i in listpp:
+    print(listpp.index("wu"))
+else:
+    print("不在列表中")
+
+
+
